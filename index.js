5570b430e2af2681751256039029905469cc4b05
Nick Landas <nick.landas@gmail.com>
2022-10-31 19:38:21 -0500

    commit tree e793601c72bc41c524381e71a921e8e89eebf174

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4;
+
+let max = Math.max(1,2,3,4,10,13,20);
